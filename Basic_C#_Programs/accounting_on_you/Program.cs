3f8de0c51f253f81c0670afe241fba1917b35e12
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-19 04:55:53 -0400

    Hello World and accounting on you assignment

---
+++ Basic_C#_Programs/accounting_on_you/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace accounting_on_you
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"Accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
