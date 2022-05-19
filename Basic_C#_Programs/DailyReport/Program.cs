1a9914e6c21adc3aeed5635f961528832f4611ac
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-19 05:45:58 -0400

    DataTypes and The Daily Report Assignment

---
+++ Basic_C#_Programs/DailyReport/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy\n" +
+                "Student Daily Report.\n" +
+                "What is your name?");
+            //This section grabs the name
+            string name = "Your name is: " + Console.ReadLine();
+            // this section grabs course name
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            //this section grabs page number as a string then convers to a short
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            int pageNum = Convert.ToInt16(page);
+            //This sections grabs true or false as string then convers to a bool
+            Console.WriteLine("Is there anything you need help with?");
+            string help = Console.ReadLine();
+            bool helpStatus = Convert.ToBoolean(help);
+            //Sections grabs positive experiences
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string exp = Console.ReadLine();
+            //This section grabs any other feedback
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            //This section grabs how many hours studies as string then convers to int
+            Console.WriteLine("How many hours did you study today?");
+            string study = Console.ReadLine();
+            int hours = Convert.ToInt16(study);
+            //-------------------------------------------------------LAST PART OF PROGRAM---------------------------------------------------
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number + 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char randomLetter = 'k';
+            char questionMark = '\u2103';
+            decimal moneyInBank = 100.5m;
+            double heightInCentimers = 211.304476;
+            float secondsLeft = 6.24f;
+            short temperatureOnMars = -341;
+            string myName = "Jeff";
+
+
+            int currentAge = 20;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = isRaining.ToString();
+
+            Console.WriteLine(questionMark);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
