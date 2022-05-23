6027603318ce7e4209d0730df5947d62ceb2b5ed
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-23 04:57:38 -0400

    DoWhile

---
+++ Basic_C#_Programs/DoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short n = 0;
+            short i = 1;
+            while(n < 10) {
+                n++;
+                Console.WriteLine("Program Itteration: " + n);
+                do
+                    {
+                    i++;
+                    Console.WriteLine("I = " + i);
+                    }
+                while(i < 8) ;
+                i = 0;
+            }
+            Console.Read();
+        }
+    }
+}
