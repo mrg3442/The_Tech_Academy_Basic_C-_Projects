530c432986946af74ff94957e8587ce24a755282
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-22 20:54:41 -0400

    Completed anon income compare

---
+++ Basic_C#_Programs/AnonIncomeCompare/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonIncomeCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            //-------------------------------------------First Person Details----------------------------------------------------------
+            Console.WriteLine("Please Enter Details for Person 1 \n" +
+                "Hourly Rate: \n");
+            string firstHourly = Console.ReadLine();
+            Console.WriteLine("\nHours worked per weak \n");
+            string firstWorked = Console.ReadLine();
+            Decimal firstRate = Convert.ToDecimal(firstHourly);
+            int firstHours = Convert.ToInt16(firstWorked);
+            Console.WriteLine("First Person Worked " + firstHours + " hours at a rate of $" + firstRate + "\n \n");
+            //-------------------------------------------Second Person Details---------------------------------------------------------
+            Console.WriteLine("Please Enter Details for Person 2 \n" +
+                "Hourly Rate: \n");
+            string secondHourly = Console.ReadLine();
+            Console.WriteLine("\nHours worked per weak \n");
+            string secondWorked = Console.ReadLine();
+            Decimal secondRate = Convert.ToDecimal(secondHourly);
+            int secondHours = Convert.ToInt16(secondWorked);
+            Console.WriteLine("First Person Worked " + secondHours + " hours at a rate of $" + secondRate);
+            //-------------------------------------------Math Operations---------------------------------------------------------------
+            int secondSalary = (int)(secondRate * secondHours * 52);
+            int firstSalary = (int)(firstRate * firstHours * 52);
+            bool firstMore;
+            if (firstSalary > secondSalary)
+            {
+                firstMore = true;
+            } else
+            {
+                firstMore = false;
+            }
+            //--------------------------------------------------Final Output--------------------------------------------------------------------
+            Console.WriteLine("\n\n\nAnnual Salary of Person 1: $" + firstSalary);
+            Console.WriteLine("\nAnnual Salary of Person 2: $" + secondSalary);
+            Console.WriteLine("\n\nDoes Person 1 make more than Person 2? \n" + firstMore);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined.ToString());
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine(difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product.ToString());
+            //Console.ReadLine();
+
+            //double quotient = 100 / 17.0;
+            //Console.WriteLine(quotient.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder.ToString());
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 2 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            short roomTemp = 70;
+            short currentTemp = 70;
+            bool isWarm = currentTemp != roomTemp;
+            Console.WriteLine(isWarm.ToString());
+            Console.ReadLine();
+
+
+        }
+    }
+}
