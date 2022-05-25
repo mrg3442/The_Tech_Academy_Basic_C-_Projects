8128c29194707126d3d3cc703cc151d5e152f837
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-25 02:38:26 -0400

    Created Array Console Assignment

---
+++ Basic_C#_Programs/ArraysAndLists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+            //int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
+            //numArray2[0] = 650;
+
+            //Console.WriteLine(numArray2[0]);
+            //Console.Read();
+
+
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+
+            Console.WriteLine(intList[0]);
+            Console.Read();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/ConsoleAppArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creates string array
+            string[] strArray = { "Bob", "Joe", "Charlie", "Jeff", "Matt" };
+            //creates int array
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            //------------------Begin of user program-----------------------------
+            //Gets user input for which array pos 1-5 then subtracts 1 to get actual array position
+            Console.WriteLine("Please select a array position of 1-5 for a string....\n");
+            int strVar = Convert.ToInt16(Console.ReadLine());
+            strVar--;
+            //Prints to console array position
+            Console.WriteLine("\nThe string you have selected is: \"" + strArray[strVar] + "\"");
+            //Gets user input for int array pos 1-5 then subtracts 1 to get array position
+            Console.WriteLine("\nPlease select a array position of 1-5 for a integer....\n");
+            int intVar = Convert.ToInt16(Console.ReadLine());
+            intVar--;
+            //Prints to console array position
+            Console.WriteLine("\nThe integer you have selected is: \"" + intArray[intVar] + "\"");
+
+
+            Console.Read();
+        }
+    }
+}
