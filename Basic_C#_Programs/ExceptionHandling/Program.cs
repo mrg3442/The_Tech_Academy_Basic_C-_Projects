bba4d122b9f6e10bbbcdc67082c5d7e82e91fb0e
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-26 01:59:26 -0400

    TryCatchBlockAssign

---
+++ Basic_C#_Programs/ExceptionHandling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two....");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.Read();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+                return;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                Console.Read();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringsAndIntegersAssign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 122, 222, 3452, 43452, 53452, 6894, 3242, 238, 94 };
+                Console.Write("Please Input a number to divide each list item by...");
+                int divideBy = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i] = numbers[i] / divideBy;
+                    Console.WriteLine("New number is...." + numbers[i]);
+                }
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Please enter a number....");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot Divide by 0....");
+            }
+            finally
+            {
+                Console.WriteLine("The program has sucesfully completed the try catch block");
+                Console.Read();
+            }
+           
+        }
+    }
+}
