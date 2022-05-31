c082392aad9648e8e5e07563199f1d8cd50486be
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-05-31 04:07:15 -0400

    CallMethodAssignment

---
+++ Basic_C#_Programs/CallMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do math operations on? \n");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var operatorObject = new Method();
+            var result = operatorObject.Multiply(num);
+            result = operatorObject.Divide(result);
+            result = operatorObject.Add(result);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallMethodAssignment/methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethodAssignment
+{
+    public class Method
+    {
+        public int Multiply(int num)
+        {
+            num = num * num;
+            return num;
+        }
+
+        public int Divide(int num)
+        {
+            num = num / 2;
+            return num;
+        }
+
+        public int Add(int num)
+        {
+            num = num + num;
+            return num;
+        }
+    }
+}
