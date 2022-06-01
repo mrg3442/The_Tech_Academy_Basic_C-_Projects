d793e1bb80f1ef7aa76b356d816e86deecd28686
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-06-01 04:32:54 -0400

    MainMethodSubmission Assignment

---
+++ Basic_C#_Programs/mainMethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethodSubmissionAssignment
+{
+    class Class1
+    {
+        //Multiplys num by itself
+        public  int MathOperation(int num)
+        {
+            num = num * num;
+            return num;
+        }
+        // second overload does same as first method but if decimal
+        public decimal MathOperation(decimal num)
+        {
+            num = num * num;
+            return num;
+        }
+        //third overloaded method takes string converts to int and does num to the power of 3
+        public int MathOperation(string num)
+        {
+            int integer = Int32.Parse(num);
+            integer = integer * integer * integer;
+            return integer;
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/mainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates Class1 
+            var operatorObject = new Class1();
+            //calls method from Class1
+            var result = operatorObject.MathOperation(2);
+            Console.WriteLine(result);
+            //creates new instantiantion of class
+            var operatorObject2 = new Class1();
+            //uses overloaded method to do a decimal instead of int
+            var result2 = operatorObject2.MathOperation(2.2m);
+            Console.WriteLine(result2);
+            // creates third instantiantion of Class1
+            var operatorObject3 = new Class1();
+            var result3 = operatorObject3.MathOperation("4");
+            Console.WriteLine(result3);
+            Console.Read();
+
+
+        }
+    }
+}
