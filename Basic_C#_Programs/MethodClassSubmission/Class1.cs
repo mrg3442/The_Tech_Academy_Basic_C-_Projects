4f54ec1fb90be5dfa5a5a8df5cd50d6e13882195
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-06-02 12:01:08 -0400

    MethodClassSubmission Assignment

---
+++ Basic_C#_Programs/MethodClassSubmission/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Class1
+    {
+        //Method that takes two ints does math op on first int and displays second
+        public void Method1(int num1, int num2)
+        {
+            num1 = num1 * num1;
+            Console.WriteLine(num2);
+
+            //int num3 = num1 * num2;
+            //Console.WriteLine(num3);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassSubmission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate class
+            var operatorObject = new Class1();
+            //calls method and specifies parameters by name
+            operatorObject.Method1(num1: 2, num2: 2);
+            Console.Read();
+        }
+    }
+}
