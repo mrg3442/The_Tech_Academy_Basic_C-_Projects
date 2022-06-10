9d1b1c5940bdb56217c7fcdd9f841afc33e52f5a
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-06-10 01:27:43 -0400

    f

---
+++ Basic_C#_Programs/MethodAndObjectSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiates new employee class then calls SayName Method
+            Employee Sample = new Employee();
+            Sample.FirstName = "Sample";
+            Sample.LastName = "Student";
+            Sample.SayName();
+            //Read Console
+            Console.Read();
+        }
+    }
+}
