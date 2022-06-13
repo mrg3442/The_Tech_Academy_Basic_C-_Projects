c120de21b980fb3a8ed07b23ac746f08e5c82dca
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-06-13 04:11:22 -0400

    OperatorsSubmission

---
+++ Basic_C#_Programs/OperatorsSubmissionAssignment/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    class Employee
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            return employee.ID == employee2.ID;
+        }
+
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee.ID != employee2.ID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var emp = obj as Employee;
+            if (emp == null)
+                return false;
+
+            return this.ID.Equals(emp.ID);
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsSubmissionAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates two instances of employee then deterimines if obj id are equal then tells user if they are or not.
+            var operatorObj = new Employee();
+            operatorObj.ID = 22;
+
+            var operatorObj2 = new Employee();
+            operatorObj2.ID = 22;
+
+            if(operatorObj == operatorObj2)
+            {
+                Console.WriteLine("They are the same");
+            }
+            else
+            {
+                Console.WriteLine("They are not the same");
+            }
+            Console.Read();
+        }
+    }
+}
