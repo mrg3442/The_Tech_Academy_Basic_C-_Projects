e6c3d50eb45cde51205fd5345bb454876844b922
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-06-15 03:04:18 -0400

    ParsingEnumsAssignment

---
+++ Basic_C#_Programs/ParameterSubmissionAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates employee w string parameter
+            var operatorObj = new Employee<string>();
+            operatorObj.things = new List<string> { "jesse", "rob", "bob" };
+            //instantiates employee with int parameter
+            var operatorObj2 = new Employee<int>();
+            operatorObj2.things = new List<int> { 1, 2, 3 };
+            
+            //Loops through firt instance of employee the string list
+            for(int i = 0;  i < operatorObj.things.Count; i++)
+            {
+                Console.WriteLine(operatorObj.things[i]);
+            }
+
+            //loops through second instance of employee the int list
+            for (int i = 0; i < operatorObj2.things.Count; i++)
+            {
+                Console.WriteLine(operatorObj2.things[i]);
+            }
+
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Basic_C#_Programs/ParseEnumSubmission/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseEnumSubmission
+{
+    public enum days
+    {
+        Monday,
+        Tuesday,
+        Wed,
+        Thursday,
+        Friday
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //gets userinput for prompt then goes through a try catch block 
+            //Try parses through enum days and if not valid asks user to input
+            //a valid day of the week.
+            Console.WriteLine("Please enter the day of the week");
+            string currentDay = Console.ReadLine();
+            
+            try
+            {
+                days day = (days)Enum.Parse(typeof(days), currentDay);
+                Console.WriteLine(day);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a actual day of the week");
+            }
+
+            Console.Read();
+        }
+    }
+}
